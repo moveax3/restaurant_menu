2a823adfef4fa88bf57a1aa9d99738970ce4df24
moveax3 <moveax3@gmail.com>
2020-10-14 21:45:14 +0500

    Add context processors

--- django/src/dishes/views.py
+++ django/src/dishes/views.py
@@ -1,20 +1,15 @@
-from django.conf import settings
 from django.shortcuts import render
 
 
 def menu_view(request):
-    # TODO: token to decorator
     return render(
         request,
         'dishes/menu.html',
-        {'API_TOKEN': settings.DRF_STATIC_TOKEN}
     )
 
 
 def summary_view(request):
-    # TODO: token to decorator
     return render(
         request,
         'dishes/summary.html',
-        {'API_TOKEN': settings.DRF_STATIC_TOKEN}
     )
---
+++ django/src/restaurant_menu/context_processor.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.urls import reverse
+
+
+def debug_context(request):
+    return {'DEBUG': settings.DEBUG}
+
+
+def api_context(request):
+    return {
+        'API_TOKEN': settings.DRF_STATIC_TOKEN,
+        'API_URLS': {
+            'dishes': reverse('dishes:api:dishes'),
+        },
+    }
+
+
+def urls_context(request):
+    return {
+        'URLS': {
+            'summary': reverse('dishes:dishes_summary'),
+            'menu': reverse('dishes:dishes_menu'),
+        },
+    }
