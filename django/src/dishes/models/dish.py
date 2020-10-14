79652c4b186d7bd51be8107a66f510c35678997a
moveax3 <moveax3@gmail.com>
2020-10-14 23:26:46 +0500

    Add new paste generation when dishes changed

--- django/src/dishes/models/dish.py
+++ django/src/dishes/models/dish.py
@@ -1,12 +1,24 @@
+import datetime
+import json
+
 from django.db import models
 from model_utils.models import TimeStampedModel
 
 from .category import Category
 from .allergen import Allergen
 
+from pastebin.tasks import post_paste
+
+
+class DishManager(models.Manager):
+    def get_json_descriptions(self) -> str:
+        dishes: list = list(self.all().values('name', 'price'))
+        return json.dumps(dishes, ensure_ascii=False)
+
 
 class Dish(TimeStampedModel):
     """ Блюдо """
+    objects = DishManager()
 
     name = models.CharField(
         max_length=128,
@@ -18,8 +30,6 @@ class Dish(TimeStampedModel):
         null=False,
         verbose_name="Цена",
     )
-
-
 
     category = models.ForeignKey(
         Category,
@@ -37,6 +47,13 @@ class Dish(TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        post_paste.delay(
+            paste_name=datetime.datetime.now(),
+            paste_text=Dish.objects.get_json_descriptions(),
+        )
 
     class Meta:
         verbose_name = "Блюдо"
